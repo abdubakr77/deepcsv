31fa9d0a2477d898d53acaa1357d612723a96e96
abdubakr77 <abdubakora1232@gmail.com>
2026-03-22 02:21:48 +0200

    Update The Version Number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="deepcsv",
-    version="0.1.0",
+    version="0.2.0",
     author="Abdullah Bakr",
     description="Automatically walks folders and converts list strings to lists in CSV/XLSX files",
     long_description=open("README.md").read(),
